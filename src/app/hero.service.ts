864ad4f7742ad7614d9a203158dc3b8b1432b841
Kelly Winzer <kelly.r.winzer@gmail.com>
2018-01-11 14:22:51 -0600

    Got it running again. Proceeding with Step 7.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of'
+
 import { MessageService } from './message.service';
 
 @Injectable()
@@ -11,10 +14,14 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(id: number): Observable<Hero[]> {
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
-
 
 }
