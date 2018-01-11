b25a6ad0e02605777b35a476b6b0a3f0c3fb5b00
Kelly Winzer <kelly.r.winzer@gmail.com>
2018-01-10 20:53:36 -0600

    3/4 of the way through step 7. Not compiling. Need break.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,9 +11,9 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  getHeroes(id: number): Observable<Hero[]> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 
 
